a75e6626899046080496dc4e5e979152d5da2e3d
CharlotteMcMain <cem24@ic.ac.uk>
2026-04-11 17:44:57 +0100

    Remove unused parsing functions and pages; delete global styles and TypeScript configuration

--- charlottemcmain_website/src/content/definitions/config.ts
+++
@@ -1,9 +0,0 @@
-import { defineCollection, z } from "astro:content";
-
-export const definitions = defineCollection({
-  type: "content",
-  schema: z.object({
-    term: z.string(),
-    topic: z.string(),
-  }),
-});
